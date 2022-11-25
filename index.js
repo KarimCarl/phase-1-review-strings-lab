5004590ef78cc782ca1f1fba4de39dce813d44f3
KarimCarl <cndungu109@gmail.com>
2022-11-25 10:50:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = 'Charles Ndungu';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`; 
+// Or use ${currentUser.slice(0,1) string method to pick character from 0 inder and before 1st index,
+//which is the first letter
